361a3f453b389964109e3002e5463329b2bbcfe5
barnameh <farimahd@gmail.com>
2016-11-22 13:51:19 -0500

    Modified link model to check and correct url if it does not start with http. Added root to the routes to show all links.

--- app/models/link.rb
+++ app/models/link.rb
@@ -3,4 +3,20 @@ class Link < ApplicationRecord
   has_many    :comments
 
   validates :url, presence: true
+
+  def url
+    if self[:url].downcase.start_with?("http")
+      self[:url]
+    else
+      "http://" + self[:url]
+    end
+  end
+
+  def url=(in_url)
+    if in_url.downcase.start_with?("http")
+      self.url = in_url
+    else
+      self.url = "http://" + in_url
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
   end
 
   resources :comments, only: [:create]
+
+  root controller: :links, action: :index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
